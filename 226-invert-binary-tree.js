a3662d0d7d4086a1245d848b1b546dde9352d3e1
tinazarb <tinazarb@gmail.com>
2022-08-07 12:24:01 -0400

    226-invert-binary-tree

---
+++ 226-invert-binary-tree.js
@@ -0,0 +1,58 @@
+// 226. Invert Binary Tree
+
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+// Example 2:
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+// Solution: Iterative
+// Time: O(n) | Space: O(n)
+
+var invertTree = function (root) {
+  const queue = [root];
+  while (queue.length) {
+    const current = queue.shift();
+    if (current === null) continue;
+    swapLeftAndRight(current);
+    queue.push(current.left);
+    queue.push(current.right);
+  }
+  return root;
+};
+
+function swapLeftAndRight(root) {
+  const left = root.left;
+  root.left = root.right;
+  root.right = left;
+}
+
+// Solution: Recursive
+// Time: O(n) | Space: O(log n)
+
+var invertTree = function (root) {
+  if (root === null) return;
+  swapLeftAndRight(root);
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
+
+function swapLeftAndRight(root) {
+  const left = root.left;
+  root.left = root.right;
+  root.right = left;
+}
