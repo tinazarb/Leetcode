e6487d30ed84d684843282233eaf1d2d685ac3fb
tinazarb <tinazarb@gmail.com>
2022-10-04 10:19:45 -0400

    973_k_closest_points_to_origin

---
+++ 973_k_closest_points_to_origin.js
@@ -0,0 +1,40 @@
+// 973. K Closest Points to Origin
+
+// Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+// The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+// You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+// Example 1:
+// Input: points = [[1,3],[-2,2]], k = 1
+// Output: [[-2,2]]
+// Explanation:
+// The distance between (1, 3) and the origin is sqrt(10).
+// The distance between (-2, 2) and the origin is sqrt(8).
+// Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+// We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+
+// Example 2:
+// Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+// Output: [[3,3],[-2,4]]
+// Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+
+var kClosest = function (points, k) {
+  const distPoints = [];
+  for (let [y, x] of points) {
+    const dist = Math.pow(y, 2) + Math.pow(x, 2);
+    distPoints.push([dist, [y, x]]);
+  }
+  distPoints.sort((a, b) => a[0] - b[0]);
+  const ans = [];
+  for (let i = 0; i < k; i++) ans.push(distPoints[i][1]);
+  return ans;
+};
+
+var kClosest = function (points, K) {
+  points.sort((a, b) => {
+    return a[0] * a[0] + a[1] * a[1] - (b[0] * b[0] + b[1] * b[1]);
+  });
+  return points.slice(0, K);
+};
