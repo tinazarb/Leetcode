e54871800eb41dbf6f7b564181b15638eb572389
tinazarb <tinazarb@gmail.com>
2022-10-11 11:00:41 -0400

    78-subsets

---
+++ 78-subsets.js
@@ -0,0 +1,27 @@
+// 78. Subsets
+
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [[],[0]]
+
+var subsets = function (nums) {
+  let result = [];
+  dfs([], 0);
+
+  function dfs(current, index) {
+    result.push(current);
+    for (let i = index; i < nums.length; i++) {
+      dfs(current.concat(nums[i]), i + 1);
+    }
+  }
+
+  return result;
+};
