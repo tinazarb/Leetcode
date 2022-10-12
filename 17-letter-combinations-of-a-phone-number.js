1fc1970017198f4e8e59b72055206a98ddc887ac
tinazarb <tinazarb@gmail.com>
2022-10-12 10:45:43 -0400

    17-letter-combinations-of-a-phone-number

---
+++ 17-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,49 @@
+// 17. Letter Combinations of a Phone Number
+
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+// Example 1:
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+// Example 2:
+// Input: digits = ""
+// Output: []
+
+// Example 3:
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+let letterCombinations = function (digits) {
+  if (!digits.length) return [];
+
+  let map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  let result = [];
+
+  function backtrack(i, curStr) {
+    if (curStr.length === digits.length) {
+      result.push(curStr);
+      return;
+    }
+
+    for (let char of map[digits[i]]) {
+      backtrack(i + 1, curStr + char);
+    }
+  }
+
+  backtrack(0, '');
+
+  return result;
+};
