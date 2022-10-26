69e32b96c6231eb5954c971c2452f033caa146ff
tinazarb <tinazarb@gmail.com>
2022-10-26 17:49:08 -0400

    reformulated solution

--- 15-3sum.js
+++ 15-3sum.js
@@ -5,7 +5,6 @@
 // Notice that the solution set must not contain duplicate triplets.
 
 // Example 1:
-
 // Input: nums = [-1,0,1,2,-1,-4]
 // Output: [[-1,-1,2],[-1,0,1]]
 // Explanation:
@@ -16,44 +15,39 @@
 // Notice that the order of the output and the order of the triplets does not matter.
 
 // Example 2:
-
 // Input: nums = [0,1,1]
 // Output: []
 // Explanation: The only possible triplet does not sum up to 0.
 
 // Example 3:
-
 // Input: nums = [0,0,0]
 // Output: [[0,0,0]]
 // Explanation: The only possible triplet sums up to 0.
 
-// Constraints:
-
-//     3 <= nums.length <= 3000
-//     -105 <= nums[i] <= 105
-
 var threeSum = function (nums) {
   nums.sort((a, b) => a - b);
   const result = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    let low = i + 1,
-      high = nums.length - 1,
-      sum = 0;
-
-    while (low < high) {
-      sum = nums[i] + nums[low] + nums[high];
-      if (sum === 0) {
-        result.push([nums[i], nums[low], nums[high]]);
-        low++;
-        high--;
-      } else if (sum < 0) low++;
-      else high--;
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break; //you can't have a+b+c =0 if sorted array's first/smallest element(a) is a positive number
+    if (i > 0 && nums[i] === nums[i - 1]) continue; //skip same numbers
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum < 0) left += 1;
+      else if (sum > 0) right -= 1;
+      else {
+        result.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left += 1;
+        while (left < right && nums[right] === nums[right - 1]) right -= 1;
+        left += 1;
+        right -= 1;
+      }
     }
-    while (nums[i + 1] === nums[i]) i++;
   }
+
   return result;
 };
 
-//Time: O(n)
+//Time: O(n^2)
 //Space: O(n)
