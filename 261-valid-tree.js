e28fc39788cab1811eba0d581cbe024aae050e95
tinazarb <tinazarb@gmail.com>
2022-12-19 15:33:40 -0500

    261-valid-tree

--- 261-valid-tree.js
+++ 261-valid-tree.js
@@ -1,53 +1,37 @@
-// 200. Number of Islands
+function validTree(n, edges) {
+  let map = {};
 
-// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
-
-// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
-
-// Example 1:
-// Input: grid = [
-//   ["1","1","1","1","0"],
-//   ["1","1","0","1","0"],
-//   ["1","1","0","0","0"],
-//   ["0","0","0","0","0"]
-// ]
-// Output: 1
-
-// Example 2:
-// Input: grid = [
-//   ["1","1","0","0","0"],
-//   ["1","1","0","0","0"],
-//   ["0","0","1","0","0"],
-//   ["0","0","0","1","1"]
-// ]
-// Output: 3
-
-const numIslands = (grid) => {
-  let count = 0;
-  for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[row].length; col++) {
-      if (grid[row][col] == '1') {
-        count++;
-        explore(row, col, grid);
-      }
+  for (let [a, b] of edges) {
+    if (!map[a]) {
+      map[a] = [];
     }
+    if (!map[b]) {
+      map[b] = [];
+    }
+    map[a].push(b);
+    map[b].push(a);
   }
-  return count;
-};
 
-function explore(row, col, grid) {
-  if (
-    row < 0 ||
-    col < 0 ||
-    row >= grid.length ||
-    col >= grid[row].length ||
-    grid[row][col] === '0'
-  ) {
-    return;
+  let visited = new Set();
+
+  if (n == 1) return true;
+  if (!dfs(0, null, visited) || visited.size < n) {
+    return false;
   }
-  grid[row][col] = '0';
-  explore(row, col + 1, grid);
-  explore(row, col - 1, grid);
-  explore(row + 1, col, grid);
-  explore(row - 1, col, grid);
+
+  return true;
+
+  function dfs(next, prev, visited) {
+    next = parseInt(next);
+
+    if (visited.has(next)) return false;
+    visited.add(next);
+    if (!map[next]) return false;
+
+    for (let child of map[next]) {
+      if (prev == child) continue;
+      if (!dfs(child, next, visited)) return false;
+    }
+    return true;
+  }
 }
