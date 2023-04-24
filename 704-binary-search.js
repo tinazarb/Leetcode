a836a7ca4e6a367e63a7ce6197ad451ebcb18a7e
tinazarb <tinazarb@gmail.com>
2023-04-24 14:29:46 -0400

    updated with alternate solution

--- 704-binary-search.js
+++ 704-binary-search.js
@@ -29,3 +29,22 @@ var search = function (nums, target) {
 
 // Time complexity: O(log n)
 // Space complexity: O(1)
+
+//Alternate Solution: accounting for left & right being large values
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+};
